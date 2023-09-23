3e15593d88d5d2df112e46758255966caf07727e
deepblue6 <joerosyleona@gmail.com>
2023-09-23 08:43:28 -0700

    Problem 7 Solution

--- problem7.c
+++ problem7.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;  // 1 and any negative number are not prime
+    }
+    if (n <= 3) {
+        return true;   // 2 and 3 are prime
+    }
+    if (n % 2 == 0 || n % 3 == 0) {
+        return false;  // Numbers divisible by 2 or 3 are not prime
+    }
+    
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return false;  // Numbers divisible by i or i+2 are not prime
+        }
+    }
+    
+    return true;  // If none of the above conditions matched, it's prime
+}
 
 int main() {
-    // 10,001st prime number
     int bigNum = 10001;
-    int running = 1;
     int primeCounter = 0;
     int c = 0;
 
-    while (running) {
+    while (1) {
         c++;
-
-        // Check if c is divisible by 2, 3, or 5
-        if (c % 2 == 0 || c % 3 == 0 || c % 5 == 0) {
-            printf("Prime No\n");
-        } else {
+        if (isPrime(c)) {
             primeCounter++;
-            printf("Prime Yes\n");
             if (primeCounter == bigNum) {
                 printf("%d\n", c);
-                break; 
+                break;
             }
         }
     }
+
+    return 0;
 }
