e604a72110fa4c6d40f7d6ac63be95fafbd5dd36
deepblue6 <joerosyleona@gmail.com>
2023-11-18 18:27:12 -0800

    Worked on Problem 14

--- Solutions/problem14.js
+++ Solutions/problem14.js
@@ -2,28 +2,88 @@
 Longest Collatz Sequence
 */
 
-let i = 0;
-let chainArray = [];
-let longestChainLength;
+// let chainArray = [];
+// let longestChainLength = 0;
 
-const collatzSequence = function (n) {
-  chainArray.push(n);
-  if (chainArray[chainArray.length - 1]) {
-    if (chainArray.length > longestChainLength) {
-      longestChainLength = chainArray.length;
-      chainArray = [];
-    }
+// const mutator = function (n) {
+//   chainArray.push(n);
+//   n % 2 === 0 ? chainArray.push(n / 2) : chainArray.push(3 * n + 1);
+//   if(!chainArray.includes())
+// };
+
+// const collatzSequence = function (n) {
+//   while (!chainArray.includes(1)) {
+//     mutator(n);
+//   }
+// };
+
+// for (let i = 0; i < 1000000; i++) {
+//   collatzSequence(i);
+// }
+// console.log(longestChainLength);
+
+// Attempt 2
+// let i = 0;
+// let chainArr = [];
+// let longestChain = 0;
+
+// const checkEven = function (num) {
+//   chainArr.push(num / 2);
+// };
+
+// const checkOdd = function (num) {
+//   chainArr.push(3 * num + 1);
+// };
+
+// console.log(checkEven(52));
+// console.log(chainArr);
+
+// // while (i < 1000000) {
+// //   for (let i = 0; i < chainArr.length; i++) {
+// //     if (chainArr[i] === 1) {
+// //       if (chainArr.length > longestChain) {
+// //         longestChain = chainArr.length;
+// //       } else {
+// //         chainArr = [];
+// //       }
+// //     }
+// //   }
+// //   if (i % 2 === 0) {
+// //     checkEven();
+// //   }
+// //   if (i % 3 === 0) {
+// //     checkOdd();
+// //   }
+
+// //   i++;
+// //   console.log(chainArr);
+// // }
+
+let numbersArray = [];
+let longestChain = 0;
+
+const checkEvenOdd = function (num) {
+  if (numbersArray.includes(1)) {
+    numbersArray = [];
   }
-  if (n % 2 === 0) {
-    chainArray.push(n / 2);
+  if (numbersArray.length > longestChain) {
+    longestChain = numbersArray.length;
   }
-  if (n % 2 === 1) {
-    chainArray.push(3 * n + 1);
+
+  if (num % 2 === 0) {
+    numbersArray.push(num);
+    numbersArray.push(num / 2);
+    num = num / 2;
+    checkEvenOdd(num);
   }
-  console.log(chainArray);
+  if (num % 2 === 1) {
+    numbersArray.push(num);
+    numbersArray.push(3 * num + 1);
+    num = 3 * num + 1;
+    checkEvenOdd(num);
+  }
 };
 
-while (i < 10) {
-  i = i + 1;
-  collatzSequence(i);
+for (let i = 10; i > 0; i--) {
+  checkEvenOdd(i);
 }
