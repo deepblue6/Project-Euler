b0226d00bc8a39d72e9c9c990d106d75fe4a97e5
deepblue6 <joerosyleona@gmail.com>
2023-11-25 18:48:59 -0800

    attempted optimization problem 5

---
+++ Solutions/problem5.js
@@ -0,0 +1,23 @@
+// for (let i = 20; i <= 232792560; i++) {
+//   if (
+//     i % 2 == 0 &&
+//     i % 3 == 0 &&
+//     i % 5 == 0 &&
+//     i % 7 == 0 &&
+//     i % 11 == 0 &&
+//     i % 13 == 0 &&
+//     i % 17 == 0 &&
+//     i % 19 == 0
+//   ) {
+//     console.log(i);
+//   }
+// }
+
+// Prime factorization
+let newNum;
+function primeFactor(num) {
+  for(let i = 2; i < num; i++) {
+    if(num % i === 0)
+    
+  }
+}
